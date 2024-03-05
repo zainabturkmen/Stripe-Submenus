05dc43703e4b6955ffaf57dde9e5fcfec5529e1c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 15:21:59 +0430

    adding some changes

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const toggleBtn = document.querySelector(".toggle-btn");
 const closeBtn = document.querySelector(".close-btn");
 const sidebarWrapper = document.querySelector(".sidebar-wrapper");
 const sidebar = document.querySelector(".sidebar-links");
-const linkBtns = document.querySelectorAll(".link-btn");
+const linkBtns = [...document.querySelectorAll(".link-btn")];
