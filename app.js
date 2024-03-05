e1580a1221c36f9639c3ee6fa2e7442b0b761c19
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-05 15:21:16 +0430

    adding some changes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 import sublinks from "./data.js"
 
 const toggleBtn = document.querySelector(".toggle-btn");
+const closeBtn = document.querySelector(".close-btn");
+const sidebarWrapper = document.querySelector(".sidebar-wrapper");
+const sidebar = document.querySelector(".sidebar-links");
+const linkBtns = document.querySelectorAll(".link-btn");
