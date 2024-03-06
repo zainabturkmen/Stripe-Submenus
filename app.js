2956fae9fe204da005934c5c57fcbe6b35f41d8f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-06 07:50:00 +0430

    adding some changes

--- app.js
+++ app.js
@@ -50,10 +50,21 @@ linkBtns.forEach((btn)=>{
             submenu.classList.add("show");
             submenu.style.left = `${center}px`;
             submenu.style.top = `${bottom}px`;
+
+
+            // optional
+            let columns = "col-2";
+            if(links.length === 3){
+                columns = "col-3"
+            }
+            if(links.length > 3){
+                columns = "col-4"
+            }
+
             submenu.innerHTML = `
             <section>
             <h4>${page}</h4>
-            <div class="submenu-center col-2">
+            <div class="submenu-center ${columns}">
             ${links.map((link)=>{
                 return `<a href="${link.url}">
                 <i class="${link.icon}"></i>${link.label}
